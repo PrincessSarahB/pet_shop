4280442002b6b5d3a6bf6959ede5e943d6e8889b
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-03-30 19:45:31 +0100

    all tests passing

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,6 +1,6 @@
 def pet_shop_name(pet_shop)
 
-return pet_shop[:name]
+  return pet_shop[:name]
 
 end
 
@@ -11,13 +11,13 @@ end
 
 def add_or_remove_cash(pet_shop, num)
 
- pet_shop[:admin][:total_cash] += num
+  pet_shop[:admin][:total_cash] += num
 
 end
 
 def pets_sold(pet_shop)
 
-return pet_shop[:admin][:pets_sold]
+  return pet_shop[:admin][:pets_sold]
 
 end
 
@@ -33,37 +33,37 @@ end
 
 def pets_by_breed(pet_shop, breed)
   pets = []
-for pet in pet_shop[:pets]
-  if pet[:breed] == breed
-  pets.push(pet_shop)
-end
-end
-return pets
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      pets.push(pet_shop)
+    end
+  end
+  return pets
 end
 
 def find_pet_by_name(pet_shop, name)
   for pet in pet_shop[:pets]
-if name == pet[:name]
-  return pet
-end
-end
-return nil
+    if name == pet[:name]
+      return pet
+    end
+  end
+  return nil
 end
 
 def remove_pet_by_name(pet_shop, name)
-for pet in pet_shop[:pets]
-  if name == pet[:name]
-    pet_shop[:pets].delete(pet)
+  for pet in pet_shop[:pets]
+    if name == pet[:name]
+      pet_shop[:pets].delete(pet)
+    end
   end
-end
 end
 
 def add_pet_to_stock(pet_shop, new_pet)
-    pet_shop[:pets].push(new_pet)
-  end
+  pet_shop[:pets].push(new_pet)
+end
 
 def customer_pet_count(customers)
-customers[:pets].count
+  customers[:pets].count
 end
 
 def add_pet_to_customer(customers, new_pet)
@@ -80,14 +80,18 @@ end
 
 def sell_pet_to_customer(pet_shop, new_pet, customer)
 
-  if new_pet
+  if new_pet && pet_shop && customer
 
-    add_pet_to_customer(customer, new_pet)
-    increase_pets_sold(pet_shop, 1)
-    add_or_remove_cash(pet_shop, new_pet[:price])
+    if customer_can_afford_pet(customer, new_pet)
+      add_pet_to_customer(customer, new_pet)
+      increase_pets_sold(pet_shop, 1)
+      add_or_remove_cash(pet_shop, new_pet[:price])
 
-return true
-else
-  return false
+      return true
+    else
+      return false
+    end
+  else
+    return false
   end
 end
